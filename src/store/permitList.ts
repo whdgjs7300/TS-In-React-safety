abefc8c68a550ee63e1111273789bfeecea21c4d
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-08-03 22:58:14 +0900

    8/3 나라페이지 링크 css 셋

--- src/store/permitList.ts
+++ src/store/permitList.ts
@@ -22,12 +22,14 @@ export const useMyStore = create<Permit>((set)=>({
         // 비동기 데이터 가져오는 로직을 구현합니다.
         try {
             set({loading : true})
+
             const response = await axios.get(`https://apis.data.go.kr/1262000/EntranceVisaService2/getEntranceVisaList2?serviceKey=${REACT_API_KEY}&returnType=JSON&numOfRows=10&pageNo=1&cond[country_nm::EQ]=${searchContry}`);
             const data = response.data.data;
         // 검색결과가 1개가 나올 땐 배열로 반환하지 않아 map 함수 실행 오류를 방지하기 위한 변수
             const dataArray = Array.isArray(data) ? data : [data];
         
             set({ permitList : dataArray});
+            
             set({ loading: false });
         } catch (error) {
             set({ loading: false });
